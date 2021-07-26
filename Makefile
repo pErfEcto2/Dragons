a3c62f8d4d5b175cd353e56ac8de1b489224e8d6
pErfEcto2 <misha.yaro2004@gmail.com>
2021-07-26 17:45:12 +0300

    Bugs fixed

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
-build_sprite:
+build_test:
 	g++ -c test.cpp
 	g++ test.o -o test -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 build_Dragons:
 	g++ -c Dragons.cpp
 	g++ Dragons.o -o Dragons -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
-build_test_collision:
-	g++ -c test_collision.cpp
-	g++ test_collision.o -o test_collision -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+
--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+/*
 #include "SFML/Graphics.hpp"
 #include <unordered_map>
 #include <iostream>
@@ -6,8 +7,9 @@ using namespace sf;
 
 int main() {
   RenderWindow renderWindow(sf::VideoMode(640, 480), "SFML Demo");
+  renderWindow.setFramerateLimit(60);
   Event event;
-  renderWindow.setKeyRepeatEnabled(false);
+  renderWindow.setKeyRepeatEnabled(true);
   unordered_map<int, bool> keys;
   vector<int> changedKeys;
   while (renderWindow.isOpen()){
@@ -32,4 +34,35 @@ int main() {
         cout << keyValue.first << " ";
     cout << endl;
   }
+}
+*/
+
+#include <SFML/System.hpp>
+#include <iostream>
+
+sf::Mutex mutex;
+
+void func()
+{
+    mutex.lock();
+
+    for (int i = 0; i < 10; ++i)
+        std::cout << "I'm thread number one" << std::endl;
+
+    mutex.unlock();
+}
+
+int main()
+{
+    sf::Thread thread(&func);
+    thread.launch();
+
+    mutex.lock();
+
+    for (int i = 0; i < 10; ++i)
+        std::cout << "I'm the main thread" << std::endl;
+
+    mutex.unlock();
+
+    return 0;
 }
